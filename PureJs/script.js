d8094ad590770b6ed13990eeee2b139248753b74
Atullya <atulmzn1@gmail.com>
2023-12-02 18:46:28 +0545

    sth changed

--- PureJs/script.js
+++ PureJs/script.js
@@ -1,15 +1,16 @@
-const arr=[1,2,3,4,5,6,7,8];
-const newMappedarray_forEq=[]
+const arr = [1, 2, 3, 4, 5, 6, 7, 8];
+const newMappedarray_forEq = [];
 
-for( let i=0; i<arr.length; i++ ){
-    const el=arr[i]
+for (let i = 0; i < arr.length; i++) {
+  const el = arr[i];
 
-
-    newMappedarray_forEq.push(el **2);
-//Line no 8 is not used more so we use11 no line
+  newMappedarray_forEq.push(el ** 2);
+  //Line no 8 is not used more so we use11 no line
 }
-const newMappedarray=arr.map(function(element){
-    console.log(element);
-    return element**2;
-})
-console.log(arr,newMappedarray)
+const newMappedarray = arr.map(function (element) {
+  console.log(element);
+  return element ** 2;
+});
+console.log(arr, newMappedarray);
+const atul = "atullya";
+console.log(atul);
